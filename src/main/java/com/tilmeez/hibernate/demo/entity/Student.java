3c79fd6877881b56e784826c0ef27ed14f2b18a9
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-05 21:22:17 +0200

    @ManyToMany - Configure Student/Course for many-to-many

--- src/main/java/com/tilmeez/hibernate/demo/entity/Student.java
+++ src/main/java/com/tilmeez/hibernate/demo/entity/Student.java
@@ -1,6 +1,7 @@
 package com.tilmeez.hibernate.demo.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "student")
@@ -16,6 +17,17 @@ public class Student {
     private String lastName;
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.DETACH,
+                    CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses;
 
     public Student() {
 
@@ -57,6 +69,14 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     @Override
