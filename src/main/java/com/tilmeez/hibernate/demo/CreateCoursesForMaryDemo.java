627c49376f9cd5cb7c870df55fdff89b728395bc
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-05 21:46:57 +0200

    @ManyToMany - Add more courses for a student

---
+++ src/main/java/com/tilmeez/hibernate/demo/CreateCoursesForMaryDemo.java
@@ -0,0 +1,69 @@
+package com.tilmeez.hibernate.demo;
+
+import com.tilmeez.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class CreateCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student mary from database
+            int theId = 6;
+            Student tempStudent = session.get(Student.class, theId);
+
+            System.out.println("\nLoaded student: " + tempStudent);
+            System.out.println("Courses: " + tempStudent.getCourses());
+
+            // create more courses
+
+            // add student to courses
+
+            // save the course
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        }finally {
+            // add clean uo code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
